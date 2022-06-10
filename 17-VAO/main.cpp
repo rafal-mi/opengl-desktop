8dfd87070ca99bf4340bbc8acfe3caf0d9eda817
Rafal Mi <rafal.m4@gmail.com>
2022-06-10 16:22:14 +0200

    Added project in 17-VAO

---
+++ 17-VAO/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "math_3d.h"
+#include "texture.h"
+#include "util.h"
+#include "camera.h"
+#include "world_transform.h"
+
+#define WINDOW_WIDTH  640
+#define WINDOW_HEIGHT 640
+
+int main(int argc, char* argv[]) {
+
+}
+
